ce384d423975a453cff0897b91c1257cbe27a5d2
jrosales2022 <juanrosales@ayudat.es>
2023-03-30 22:35:46 +0200

    first commit

---
+++ assets/js/juego.js
@@ -0,0 +1,31 @@
+/* 
+    C -> Clubs (Tréboles)
+    D -> Diamons (Diamantes)
+    H -> Hearts (Corazones)
+    S -> Spades (Espadas)
+*/
+
+const types = ['C','D','H','S']
+const specials = ['A','J','Q','K']
+let desk = [];
+
+
+/*
+Este código define una función llamada crearDesk
+que crea y devuelve una baraja de cartas mezclada.
+*/
+const crearDesk = () => {
+
+    for( let i = 2; i <= 10; i++ ){
+        for (let type of types) {
+            desk.push(`${i}${type}`);
+        }
+    }
+   for (let type of types) {
+    for (let special of specials) {
+        desk.push(`${special}${type}`);
+    }
+   }
+    return _.shuffle(desk)
+}
+
