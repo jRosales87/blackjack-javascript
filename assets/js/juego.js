66cb9efd0f6dc66647686ce5d450951395830fbf
jrosales2022 <juanrosales@ayudat.es>
2023-05-03 18:42:40 +0200

    feat(juego.js): añadir funcionalidad de pedir carta y detener juego feat(index.html): añadir id a los botones de juego y cambiar texto de botón nuevo juego

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,6 +8,15 @@
 const types = ['C','D','H','S']
 const specials = ['A','J','Q','K']
 let desk = [];
+let puntosJ = 0, puntosB = 0;
+
+
+//Referencias 
+const btnPedir = document.querySelector('#btnPedir'),
+      btnDetener = document.querySelector('#btnDetener'),
+      smalls = document.querySelectorAll('small'),
+      jugadorCartas = document.querySelector('#jugador-cartas'),
+      bancaCartas = document.querySelector('#banca-cartas');
 
 
 /*
@@ -39,4 +48,72 @@ const pedirCarta = () => {
     return desk.pop();
 }
 
-pedirCarta();
+// pedirCarta();
+
+const valorCarta = (carta) => {
+
+    const valor = carta.substring(0, carta.length-1);
+    return ( isNaN(valor) ) ? ((valor == 'A') ? 11 : 10) : valor * 1;
+}
+
+
+const turnoBanca = ( puntosMinimos ) => {
+
+do {
+    const carta = pedirCarta();
+    let img = document.createElement('img');
+    
+    puntosB = puntosB + valorCarta( carta );
+    smalls[1].innerHTML=puntosB;
+    img.src = `assets/cartas/${carta}.png`
+    img.classList.add('__carta');
+    bancaCartas.append(img);
+
+    if( puntosMinimos > 21 ){
+        break;
+    }
+    
+} while ( (puntosB < puntosMinimos) && (puntosMinimos <= 21 ) );
+
+
+
+
+}
+
+
+// Eventos
+
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+    let img = document.createElement('img');
+    
+    puntosJ = puntosJ + valorCarta( carta );
+    smalls[0].innerHTML=puntosJ;
+    img.src = `assets/cartas/${carta}.png`
+    img.classList.add('__carta');
+    jugadorCartas.append(img);
+
+    
+    if( puntosJ > 21 ) {
+        console.warn('Perdiste Bro');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoBanca( puntosJ );
+    } else if( puntosJ === 21 ){
+        console.warn('21, genial!');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoBanca( puntosJ );
+    }
+
+
+})
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoBanca( puntosJ );
+})
+
+
+
